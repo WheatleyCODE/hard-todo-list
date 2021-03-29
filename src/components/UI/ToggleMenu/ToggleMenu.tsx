11e985042360d565c6f27fe128f045f79e0845fe
WheatleyCODE <WheatleyCODE@users.noreply.github.com>
2021-03-29 15:54:56 +0300

    ToggleMenu fix

--- src/components/UI/ToggleMenu/ToggleMenu.tsx
+++ src/components/UI/ToggleMenu/ToggleMenu.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { MenuItemGenerator } from '../../../utils/utils'
-import { MenuItemElement } from '../../../types/types'
+import { MenuItemGenerator, MenuItemList } from '../../../utils/utils'
 import './ToggleMenu.scss'
 
 type toggleMenuProps = {
@@ -8,44 +7,6 @@ type toggleMenuProps = {
 }
 
 const ToggleMenu = ({ onToggle }: toggleMenuProps) => {
-
-  const MenuItemList: MenuItemElement[] = [
-    {
-      path: '/',
-      icon: 'fa fa-home',
-      text: 'Home'
-    },
-    {
-      path: '/todo',
-      icon: 'fa fa-check-square-o',
-      text: 'Todo'
-    },
-    {
-      path: '/red',
-      icon: 'fa fa-circle red',
-      text: 'Red'
-    },
-    {
-      path: '/yellow',
-      icon: 'fa fa-circle yellow',
-      text: 'Yellow'
-    },
-    {
-      path: '/green',
-      icon: 'fa fa-circle green',
-      text: 'Green'
-    },
-    {
-      path: '/grey',
-      icon: 'fa fa-circle grey',
-      text: 'Grey'
-    },
-    {
-      path: '/seatings',
-      icon: 'fa fa-sliders',
-      text: 'Seatings'
-    },
-  ]
 
   return (
     <div className="toggle-menu">
--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -18,3 +18,41 @@ export const MenuItemGenerator = (elem: MenuItemElement[], onToggle: () => void)
 
   return menuItems
 }
+
+export const MenuItemList: MenuItemElement[] = [
+  {
+    path: '/',
+    icon: 'fa fa-home',
+    text: 'Home'
+  },
+  {
+    path: '/todo',
+    icon: 'fa fa-check-square-o',
+    text: 'Todo'
+  },
+  {
+    path: '/red',
+    icon: 'fa fa-circle red',
+    text: 'Red'
+  },
+  {
+    path: '/yellow',
+    icon: 'fa fa-circle yellow',
+    text: 'Yellow'
+  },
+  {
+    path: '/green',
+    icon: 'fa fa-circle green',
+    text: 'Green'
+  },
+  {
+    path: '/grey',
+    icon: 'fa fa-circle grey',
+    text: 'Grey'
+  },
+  {
+    path: '/seatings',
+    icon: 'fa fa-sliders',
+    text: 'Seatings'
+  },
+]
